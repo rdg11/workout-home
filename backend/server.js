65a4c63a16c05b661be809020c7bfea1f442786a
rdg11 <rgoodbody11@gmail.com>
2023-07-25 20:46:25 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -24,8 +24,11 @@ mongoose
 	.connect(process.env.MONGO_URI)
 	.then(() => {
 		// listen for requests
-		app.listen(process.env.PORT, () => {
-			console.log('connected to db & listening on port', process.env.PORT)
+		app.listen(process.env.PORT || 4000, () => {
+			console.log(
+				'connected to db & listening on port',
+				process.env.PORT || 4000
+			)
 		})
 	})
 	.catch(error => {
